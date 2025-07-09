85ecc72f7c74baeb2711c5dca550deaeac9f81f5
KevR98 <ramil.kevin98@gmail.com>
2025-07-09 17:28:02 +0200

    Fixed the cards and remove btn

--- assets/script/script.js
+++ assets/script/script.js
@@ -15,20 +15,28 @@ const getBooks = function () {
       console.log(bookObj[0]);
 
       const book = document.getElementById('cardBook');
-      for (let i = 0; i < bookObj.length; i++) {
+      bookObj.forEach((bookItem) => {
         book.innerHTML += `
         <div class="col col-12 col-sm-6 col-md-3">
-            <div class="card mb-3">
-                <img src="${bookObj[i].img}" class="card-img-top" alt="Pandemic">
+            <div class="card mb-3 h-100">
+                <img src="${bookItem.img}" class="card-img-top" alt="Pandemic">
                     <div class="card-body">
-                    <h2 class="card-title">${bookObj[i].title}</h2>
-                    <h5 class="card-text">Price: ${bookObj[i].price}</h5>
-                    <a href="#" class="btn btn-danger">SCARTA</a>
+                    <h2 class="card-title">${bookItem.title}</h2>
+                    <p class="card-text fs-2">Price: ${bookItem.price}</p>
+                    <a href="#" class="btn btn-danger remove">SCARTA</a>
                 </div>
             </div>
         </div>
       `;
-      }
+      });
+      const removeBtn = document.querySelectorAll('.remove');
+      removeBtn.forEach((btn) => {
+        btn.addEventListener('click', function (e) {
+          // Trova il genitore colonna della card e rimuovilo
+          const cardCol = e.target.closest('.col');
+          if (cardCol) cardCol.remove();
+        });
+      });
     })
     .catch((err) => {
       console.log('ERRORE!', err);
