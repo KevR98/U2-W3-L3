2a01dd03adb2821bc292d869bf29af4740f786a2
KevR98 <ramil.kevin98@gmail.com>
2025-07-09 21:25:04 +0200

    fixed

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,36 +5,40 @@ const getBooks = function () {
     .then((res) => {
       console.log('RESPONSE', res);
       if (res.ok) {
+        // Faccio un controllo... e se il server è OK, esprapolo il JSON
         return res.json();
       } else {
+        // Il Response ha un problema!
         throw new Error('LA RESPONSE DA STRIVESCHOOL NON È OK');
       }
     })
     .then((bookObj) => {
-      console.log(bookObj);
-      console.log(bookObj[0]);
-
+      // Richiamo in una variabile il div con id "cardBook"
       const book = document.getElementById('cardBook');
       bookObj.forEach((bookItem) => {
+        // Creo l'HTML con += (Ovvero che aggiunge)
         book.innerHTML += `
         <div class="col col-12 col-sm-6 col-md-3">
             <div class="card mb-3 h-100">
                 <img src="${bookItem.img}" class="card-img-top" alt="Pandemic">
                     <div class="card-body">
-                    <h2 class="card-title">${bookItem.title}</h2>
-                    <p class="card-text fs-2">Price: ${bookItem.price}</p>
-                    <a href="#" class="btn btn-danger remove">SCARTA</a>
+                    <h5 class="card-title">${bookItem.title}</h5>
+                    <p class="card-text fs-5">Price: ${bookItem.price}€</p>
+                    <button class="btn btn-danger remove">SCARTA</button>
                 </div>
             </div>
         </div>
       `;
       });
-      const removeBtn = document.querySelectorAll('.remove');
-      removeBtn.forEach((btn) => {
+
+      // Assegno la variabile removeCard alla classe con bottone remove
+      const removeCard = document.querySelectorAll('.remove');
+      // Faccio un ciclo forEach dove assegno un parametro "btn"
+      removeCard.forEach((btn) => {
+        // Creo l'evento listener che al click del bottone (usato come parametro)
         btn.addEventListener('click', function (e) {
-          // Trova il genitore colonna della card e rimuovilo
-          const cardCol = e.target.closest('.col');
-          if (cardCol) cardCol.remove();
+          // Va a cercare il padre più vicino a ".col"... gli assegna la classe d-none
+          btn.closest('.col').classList.add('d-none');
         });
       });
     })
